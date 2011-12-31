cb343cb163e573009ba3f1edcb728ffcacb50cab
Fikri Rasyid <fikrirasyid@gmail.com>
2011-12-31 07:49:38 +0700

    Pantomime Init

---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="no-js ie6 oldie" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+	<title>Pantomime Theme</title>
+
+	<meta name="viewport" content="width=device-width,initial-scale=1">
+
+	<link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<div id="container">
+	<header>
+
+	</header>
+	<div id="main" role="main">
+
+	</div>
+	<footer>
+
+	</footer>
+</div> <!--! end of #container -->
+
+</body>
+</html>
