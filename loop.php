83b01079d032e4c7681e6e379b62465ec603c6e2
Fikri Rasyid <fikrirasyid@gmail.com>
2012-01-01 21:59:10 +0700

    Adding error 404 notification.

--- loop.php
+++ loop.php
@@ -2,7 +2,13 @@
     // No post found, baby
     if (!have_posts()) :
 ?>
-
+    <article>
+        <h1 class="title"><?php _e( "Wait a sec: You're Lost", "pantomime" ); ?></h1>
+        <div class="content">
+            <p><?php _e( "I'm sorry to say this but the page you are trying to access is not exist.", 'pantomime' ); ?></p>
+            <p><?php _e( "You want to go back to the <a href='". get_bloginfo('url') ."'>homepage</a>, perhaps?.", 'pantomime' );?></p>
+        </div>
+    </article>
 <?php
     endif;
     
