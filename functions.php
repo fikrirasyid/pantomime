d57f49176edcf2a0faba039405b3424bd86ec994
Fikri Rasyid <fikrirasyid@gmail.com>
2012-01-01 12:17:41 +0700

    Better link scheme & content title heading optimisation

--- functions.php
+++ functions.php
@@ -298,7 +298,7 @@ function pantomime_content(){
 	?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 		<?php if( is_single() ): ?>
-                <h1 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                <h1 class="title"><?php the_title(); ?></h1>
 		<?php else: ?>
                 <h2 class="title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>		
 		<?php endif;?>
